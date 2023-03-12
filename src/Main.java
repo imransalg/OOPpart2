4778bba7563807c1217a1ea35deae7a497cac5b9
imransalg <imransalgiriev5200@gmail.com>
2023-03-12 09:52:11 +0300

    OOP part 2 done

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void printSeparator() {
+        System.out.println("----------------");
+    }
+    public static void main(String[] args) {
+        Car car =  new Car("car1", 4);
+        Car car2 = new Car("car2", 4);
+
+        Truck truck = new Truck("truck1", 6);
+        Truck truck2 = new Truck("truck2", 8);
+
+        Bicycle bicycle = new Bicycle("bicycle1", 2);
+        Bicycle bicycle2 = new Bicycle("bicycle2", 2);
+
+        ServiceStation station = new ServiceStation();
+
+        station.check(car);
+        printSeparator();
+        station.check(car2);
+        printSeparator();
+
+        station.check(bicycle);
+        printSeparator();
+        station.check(bicycle2);
+        printSeparator();
+
+        station.check(truck);
+        printSeparator();
+        station.check(truck2);
+        printSeparator();
+
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,27 @@
+import javax.naming.Name;
+
+public class Vehicle {
+    private String modelName;
+    private int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+}
